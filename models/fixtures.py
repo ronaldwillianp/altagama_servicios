979d249f4d4d5454746afb554358d0cac11b3e93
Ronald Willian Perez Sanchez <ronaldwillianp@gmail.com>
2024-11-22 11:38:48 -0500

    Creando pruebas

--- models/fixtures.py
+++ models/fixtures.py
@@ -3,6 +3,7 @@
 # db.auth_group.truncate()
 # db.auth_membership.truncate()
 
+
 # # Agregando roles de usuario
 # db.auth_group.validate_and_insert(role = 'Administrador', description = 'Administrador')
 # db.auth_group.validate_and_insert(role = 'Administrativo', description = 'Administrativo')
@@ -34,3 +35,25 @@
 # db.auth_membership.validate_and_insert(user_id = 8, group_id = 5)
 # db.auth_membership.validate_and_insert(user_id = 9, group_id = 5)
 
+# db.notificacion.truncate()
+# db.mantenimiento.truncate()
+# db.mantenimiento_contrato.truncate()
+# db.firma_autorizada.truncate()
+# db.contacto.truncate()
+# db.contrato.truncate()
+
+# db.contrato.validate_and_insert(numero=1, empresa='EMPRESA INFORMATICA AUTOMATICA Y COMUNICACIONES', naturaleza='sv', tipo_contrato='cl', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*5)))
+# db.contrato.validate_and_insert(numero=2, empresa='EMPRESA DE INFORMÁTICA Y MEDIOS AUDIOVISUALES', naturaleza='sv', tipo_contrato='pv', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*5)))
+# db.contrato.validate_and_insert(numero=3, empresa='LiangWare', naturaleza='sv', tipo_contrato='pv', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*3)))
+# db.contrato.validate_and_insert(numero=4, empresa='Ecoverd', naturaleza='sv', tipo_contrato='cl', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*4)))
+# db.contrato.validate_and_insert(numero=5, empresa='xprad', naturaleza='sv', tipo_contrato='cl', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*2)))
+# db.contrato.validate_and_insert(numero=6, empresa='ESPOLETA Tecnologías', naturaleza='sv', tipo_contrato='cl', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*5)))
+# db.contrato.validate_and_insert(numero=7, empresa='DOFLEINI SOFTWARE', naturaleza='sv', tipo_contrato='cl', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*3)))
+# db.contrato.validate_and_insert(numero=8, empresa='Kairos', naturaleza='sv', tipo_contrato='cl', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*3)))
+# db.contrato.validate_and_insert(numero=9, empresa='PAXIOLI', naturaleza='sv', tipo_contrato='cl', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*6)))
+# db.contrato.validate_and_insert(numero=10, empresa='Guajiritos S.R.L.', naturaleza='vt', tipo_contrato='pv', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*4)))
+# db.contrato.validate_and_insert(numero=11, empresa='RENOVA', naturaleza='vt', tipo_contrato='pv', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*3)))
+# db.contrato.validate_and_insert(numero=12, empresa='CALISOFT', naturaleza='sv', tipo_contrato='cl', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*5)))
+# db.contrato.validate_and_insert(numero=13, empresa='Más Uno S.R.L.', naturaleza='vt', tipo_contrato='pv', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*5)))
+# db.contrato.validate_and_insert(numero=14, empresa='ALOJA S.U.R.L', naturaleza='sv', tipo_contrato='pv', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*2)))
+# db.contrato.validate_and_insert(numero=15, empresa='HabaMarketHM surl', naturaleza='sv', tipo_contrato='pv', estado_contrato='ar', fecha_vencimiento=(datetime.date.today()+datetime.timedelta(days=365*3)))
